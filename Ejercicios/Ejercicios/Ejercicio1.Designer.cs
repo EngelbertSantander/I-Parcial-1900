c243811b560be1044f1e915e69a3d0874dd44df3
EngelbertSantander <124478151+EngelbertSantander@users.noreply.github.com>
2023-02-23 20:29:40 -0600

    Ejercicios Iniciales

---
+++ Ejercicios/Ejercicios/Ejercicio1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Ejercicios
+{
+    partial class Ejercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Num1TextBox = new System.Windows.Forms.TextBox();
+            this.Num2TextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CalcularButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1:";
+            // 
+            // Num1TextBox
+            // 
+            this.Num1TextBox.Location = new System.Drawing.Point(161, 71);
+            this.Num1TextBox.Name = "Num1TextBox";
+            this.Num1TextBox.Size = new System.Drawing.Size(237, 22);
+            this.Num1TextBox.TabIndex = 1;
+            // 
+            // Num2TextBox
+            // 
+            this.Num2TextBox.Location = new System.Drawing.Point(161, 136);
+            this.Num2TextBox.Name = "Num2TextBox";
+            this.Num2TextBox.Size = new System.Drawing.Size(237, 22);
+            this.Num2TextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Número 2:";
+            // 
+            // CalcularButton
+            // 
+            this.CalcularButton.Location = new System.Drawing.Point(161, 188);
+            this.CalcularButton.Name = "CalcularButton";
+            this.CalcularButton.Size = new System.Drawing.Size(237, 44);
+            this.CalcularButton.TabIndex = 4;
+            this.CalcularButton.Text = "Calcular";
+            this.CalcularButton.UseVisualStyleBackColor = true;
+            this.CalcularButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 300);
+            this.Controls.Add(this.CalcularButton);
+            this.Controls.Add(this.Num2TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Num1TextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Num1TextBox;
+        private System.Windows.Forms.TextBox Num2TextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button CalcularButton;
+    }
+}
+
---
+++ Ejercicios/Ejercicios/Ejercicio2.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Ejercicios
+{
+    partial class Ejercicio2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalcularButton = new System.Windows.Forms.Button();
+            this.Num2TextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Num1TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OperacionesComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // CalcularButton
+            // 
+            this.CalcularButton.Location = new System.Drawing.Point(299, 293);
+            this.CalcularButton.Name = "CalcularButton";
+            this.CalcularButton.Size = new System.Drawing.Size(237, 44);
+            this.CalcularButton.TabIndex = 9;
+            this.CalcularButton.Text = "Calcular";
+            this.CalcularButton.UseVisualStyleBackColor = true;
+            this.CalcularButton.Click += new System.EventHandler(this.CalcularButton_Click);
+            // 
+            // Num2TextBox
+            // 
+            this.Num2TextBox.Location = new System.Drawing.Point(299, 199);
+            this.Num2TextBox.Name = "Num2TextBox";
+            this.Num2TextBox.Size = new System.Drawing.Size(237, 22);
+            this.Num2TextBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Número 2:";
+            // 
+            // Num1TextBox
+            // 
+            this.Num1TextBox.Location = new System.Drawing.Point(299, 134);
+            this.Num1TextBox.Name = "Num1TextBox";
+            this.Num1TextBox.Size = new System.Drawing.Size(237, 22);
+            this.Num1TextBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Número 1:";
+            // 
+            // OperacionesComboBox
+            // 
+            this.OperacionesComboBox.FormattingEnabled = true;
+            this.OperacionesComboBox.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicación",
+            "División"});
+            this.OperacionesComboBox.Location = new System.Drawing.Point(299, 247);
+            this.OperacionesComboBox.Name = "OperacionesComboBox";
+            this.OperacionesComboBox.Size = new System.Drawing.Size(237, 24);
+            this.OperacionesComboBox.TabIndex = 10;
+            // 
+            // Ejercicio2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OperacionesComboBox);
+            this.Controls.Add(this.CalcularButton);
+            this.Controls.Add(this.Num2TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Num1TextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio2";
+            this.Text = "Ejercicio2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CalcularButton;
+        private System.Windows.Forms.TextBox Num2TextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Num1TextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox OperacionesComboBox;
+    }
+}
